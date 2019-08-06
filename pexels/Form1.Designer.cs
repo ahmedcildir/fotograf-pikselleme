1aefb586d253320ef15111262f75534874afc01b
ahmed Jaldar <ahmedcildir1@gmail.com>
2019-08-06 14:35:26 +0300

    v1

---
+++ pexels/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace pexels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 328);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "iceleme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(684, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 316);
+            this.listBox1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(236, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(218, 328);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "değişti";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(460, 45);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(218, 323);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(684, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(20, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "değişti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(12, 375);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(218, 323);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(255, 11);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "değişti";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 703);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ pexels/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pexels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //pexels x = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+                this.Text = openFileDialog1.FileName;
+                
+               
+                
+            }
+            catch
+            { 
+            
+            }
+        }
+        int ort;
+        int genelOrt;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Bitmap bix = new Bitmap(pictureBox1.Image);
+            Color cobi;
+
+            int a = 0,r = 0, g = 0, b = 0;
+            int n = bix.Width * bix.Height;
+            
+
+
+                for (int i = 0; i <bix.Width ; i++)
+                {
+                    for (int j = 0; j <bix.Height ; j++)
+                    {
+                        cobi = bix.GetPixel(i, j);
+                        a = a + cobi.A;
+                        r = r + cobi.R;
+                        g = r + cobi.G;
+                        b = b + cobi.B;
+                    }
+                }
+
+            listBox1.Items.Add("pixel sayısı: " + bix.Width + "x" + bix.Height);
+
+            ort = a / n;
+            genelOrt=genelOrt+ort;
+
+            listBox1.Items.Add("A rengi ort: " + ort);
+
+            ort = r / n;
+            genelOrt=genelOrt+ort;
+
+            listBox1.Items.Add("Kırmızı rengi ort: " + ort);
+
+            ort = g / n;
+            genelOrt=genelOrt+ort;
+
+            listBox1.Items.Add("Yaşil rengi ort: " + ort);
+
+            ort = b / n;
+            genelOrt=genelOrt+ort;
+
+            listBox1.Items.Add("Mavi rengi ort: " + ort);
+            
+            genelOrt=(genelOrt+ort)/3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bix = new Bitmap(pictureBox1.Image);
+            Color cobi;
+            for (int i = 0; i < bix.Width; i++)
+            {
+                for (int j = 0; j < bix.Height; j++)
+                {
+                    cobi = bix.GetPixel(i, j);
+                    int ort = (cobi.R + cobi.G + cobi.B) / 3;
+                    bix.SetPixel(i, j, Color.FromArgb(cobi.A, ort, ort, ort));
+                }
+            }
+            pictureBox2.Image = bix;
+
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bix = new Bitmap(pictureBox1.Image);
+            Color cobi;
+            for (int i = 0; i < bix.Width/4; i++)
+            {
+                for (int j = 0; j < bix.Height/4; j++)
+                {
+                    cobi = bix.GetPixel(i, j);
+                    int ort = (cobi.R + cobi.G + cobi.B) / 3;
+                    listBox1.Items.Add(i+","+j+": "+ort);
+                    
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap bix = new Bitmap(pictureBox1.Image);
+            Color cobi;
+            for (int i = 0; i < bix.Width; i++)
+            {
+                for (int j = 0; j < bix.Height; j++)
+                {
+                    cobi = bix.GetPixel(i, j);
+                    int ort = (cobi.R + cobi.G + cobi.B) / 3;
+                    
+                    if(ort<genelOrt)
+                    {
+                        ort=ort/2;
+                        bix.SetPixel(i, j, Color.FromArgb(cobi.A, cobi.R+ort, cobi.G+ort, cobi.B+ort));
+                    }
+                    else if(ort>genelOrt)
+                    {
+                        ort=ort/2;
+                        bix.SetPixel(i, j, Color.FromArgb(cobi.A, cobi.R-ort, cobi.G-ort, cobi.B-ort));
+                    }
+                }
+            }
+            pictureBox2.Image = bix;
+        }
+    }
+}
